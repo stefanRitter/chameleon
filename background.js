6d52e29917ce013f2e5a1c36d8ddc02e85a82821
stefanRitter <stefan@stefanritter.com>
2014-01-12 00:03:20 +0000

    background

---
+++ background.js
@@ -0,0 +1,34 @@
+// Cameleon v0.0.1 by Stefan Ritter
+// MIT Licesense
+// github.com/stefanRitter/cameloen
+
+// background.js:
+// 1. opens a new tab
+// 2. sends page url and devices options
+// 3. listens for and sends refresh messages
+
+
+var cameleonTab = {},
+    devices = [{
+      type: 'ios',
+      title: 'iPhone 4',
+      x: 320,
+      y: 480
+    },
+    {
+      type: 'ios',
+      title: 'iPhone 5',
+      x: 320,
+      y: 568
+    }];
+
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.create({'url': chrome.extension.getURL('cameleon.html')}, function (newTab) {
+    cameleonTab = newTab;
+    chrome.tabs.sendMessage(cameleonTab.id, {
+      url: tab.url,
+      devices: devices
+    }, function (response) {});
+  });
+});
