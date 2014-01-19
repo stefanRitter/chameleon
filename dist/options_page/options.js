f198a026aaeae68ebfa9d2848d4e66f44738be68
stefanRitter <stefan@stefanritter.com>
2014-01-19 10:20:47 +0000

    remove $

--- dist/options_page/options.js
+++ dist/options_page/options.js
@@ -1,16 +1,29 @@
 
 (function () {
   'use strict';
-  var $devices = document.getElementById("devices"),
+  var devices = document.getElementById("devices"),
       template = document.getElementById('device-template').querySelector('.device');
+
+
+  function removeDevice (event) {
+    var device = event.target.parentNode;
+    device.parentNode.removeChild(device);
+  }
+
+
+  function addDevice (event, device) {
+    device = device || template.cloneNode(true);
+    device.addEventListener('click', removeDevice);
+    devices.appendChild(device);
+  }
+
 
   // Saves devices to localStorage.
   function saveOptions() {
-    for (var i = 0, len = $devices.children.length; i < len; i++) {
-      var device = $devices.children[i];
+    for (var i = 0, len = devices.children.length; i < len; i++) {
+      var device = devices.children[i];
       console.log(device);
     }
-
 
     // Let user know options were saved.
     var status = document.getElementById("status");
@@ -23,37 +36,23 @@
 
   // Restores previously created devices from localStorage.
   function restoreOptions() {
-    var devices = JSON.parse(localStorage['cameleon_devices']);
+    var savedDevices = JSON.parse(localStorage['cameleon_devices']);
 
-    if (!devices) {
+    if (!savedDevices) {
       return;
     }
     
-    devices.forEach(function (device) {
-      var $device = template.cloneNode(true),
-          $inputs = $device.querySelectorAll('input'),
-          $typeSelect = $device.querySelector('select');
+    savedDevices.forEach(function (device) {
+      var deviceNode = template.cloneNode(true),
+          inputs = deviceNode.querySelectorAll('input'),
+          typeSelect = deviceNode.querySelector('select');
 
-      $inputs[0].value = device.title;
-      $inputs[1].value = device.x;
-      $inputs[2].value = device.y;
+      inputs[0].value = device.title;
+      inputs[1].value = device.x;
+      inputs[2].value = device.y;
 
+      addDevice(null, deviceNode);
     });
-  }
-
-
-  // remove device
-  function removeDevice (event) {
-    var device = event.target.parentNode;
-    device.parentNode.removeChild(device);
-  }
-
-
-  // add a device to the options list
-  function addDevice (event, device) {
-    device = device || template.cloneNode(true);
-    device.addEventListener('click', removeDevice);
-    $devices.appendChild(device);
   }
 
 
