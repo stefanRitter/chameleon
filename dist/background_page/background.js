c23121823b2516efda679c4799d77194ced55a2a
Stefan Ritter <stefan.ritter@sap.com>
2017-03-15 22:33:54 +0100

    update zepto

--- dist/background_page/background.js
+++ dist/background_page/background.js
@@ -7,74 +7,80 @@
 // 2. sends page url and devices options
 // 3. listens for updates and sends refresh messages
 
+chrome.runtime.onInstalled.addListener(function () {
+    'use strict';
+
+    var defaultDevices = [
+        {
+            type: 'ios',
+            title: 'iPhone 5',
+            x: 320,
+            y: 568
+        },
+        {
+            type: 'android',
+            title: 'HTC one',
+            x: 360,
+            y: 640
+        },
+        {
+            type: 'ios',
+            title: 'iPhone 7+',
+            x: 414,
+            y: 736
+        },
+        {
+            type: 'ios',
+            title: 'iPad mini',
+            x: 768,
+            y: 1024
+        }
+    ];
+
+
+    if (!localStorage['chameleon_devices_v3']) {
+        localStorage.removeItem('chameleon_devices');
+        localStorage.removeItem('chameleon_devices_v2');
+        localStorage['chameleon_devices_v3'] = JSON.stringify(defaultDevices);
+    }
+});
+
+
 
 chrome.browserAction.onClicked.addListener(function (tab) {
-  'use strict';
+    'use strict';
 
-  var chameleonTab = {id: 0},
-      originalTab = tab,
-      defaultDevices = [
-      {
-        type: 'ios',
-        title: 'iPhone 5',
-        x: 320,
-        y: 568
-      },
-      {
-        type: 'android',
-        title: 'HTC one',
-        x: 360,
-        y: 640
-      },
-      {
-        type: 'ios',
-        title: 'iPhone 7+',
-        x: 414,
-        y: 736
-      },
-      {
-        type: 'ios',
-        title: 'iPad mini',
-        x: 768,
-        y: 1024
-      }];
+    var chameleonTab = {id: 0};
+    var originalTab = tab;
+
+    chrome.tabs.create({'url': chrome.extension.getURL('chameleon_page/chameleon.html')}, function (newTab) {
+        var devices = JSON.parse(localStorage['chameleon_devices_v3']);
+
+        chameleonTab = newTab;
+        setTimeout( function () {
+            chrome.tabs.sendMessage(chameleonTab.id, {
+                url: tab.url,
+                devices: devices
+            }, function (response) {});
+        }, 500);
+    });
 
 
-  if (!localStorage['chameleon_devices_v3']) {
-    localStorage.removeItem('chameleon_devices');
-    localStorage.removeItem('chameleon_devices_v2');
-    localStorage['chameleon_devices_v3'] = JSON.stringify(defaultDevices);
-  }
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        var updateIframes = function () {
+            chrome.tabs.sendMessage(chameleonTab.id, {
+                update: true,
+                url: originalTab.url
+            }, function (response) {});
+        };
 
-
-  chrome.tabs.create({'url': chrome.extension.getURL('chameleon_page/chameleon.html')}, function (newTab) {
-    var devices = JSON.parse(localStorage['chameleon_devices_v3']);
-
-    chameleonTab = newTab;
-    setTimeout( function () {
-      chrome.tabs.sendMessage(chameleonTab.id, {
-        url: tab.url,
-        devices: devices
-      }, function (response) {});
-    }, 500);
-  });
-
-
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    var updateIframes = function () {
-      chrome.tabs.sendMessage(chameleonTab.id, {
-        update: true,
-        url: originalTab.url
-      }, function (response) {});
-    };
-
-    if (tab.id === originalTab.id) {
-      if ('url' in changeInfo) {
-        originalTab.url = changeInfo.url;
-        updateIframes();
-      } else if ('status' in changeInfo && changeInfo.status === 'loading') {
-        updateIframes();
-      }
-    }
-  });
+        if (tab.id === originalTab.id) {
+            if ('url' in changeInfo) {
+                originalTab.url = changeInfo.url;
+                updateIframes();
+            } else if ('status' in changeInfo && changeInfo.status === 'loading') {
+                updateIframes();
+            }
+        }
+    });
 });
